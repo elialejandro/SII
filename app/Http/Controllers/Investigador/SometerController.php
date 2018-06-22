2f8072e74518234d11d70a4bd3be23bc14103c78
I.S.C. Jorge Octavio Guzmán <jogs78@gmail.com>
2018-06-22 02:16:20 -0500

    Solo mostrar no sometidos

--- app/Http/Controllers/Investigador/SometerController.php
+++ app/Http/Controllers/Investigador/SometerController.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Storage;
 
 
 use App\Models\Proyecto;
+use App\Models\Convocatoria;
+
 use App\Models\Vinculacion;
 //use App\Models\User;
 
@@ -28,8 +30,46 @@ class SometerController extends Controller
 
     public function someter($idproy)
     {
+        $validacion = array(
+            'convocatoria' => array('resultado' => "resultado", 'mensaje' => "mensaje", ) , 
+            'rubro' => array('resultado' => "resultado", 'mensaje' => "mensaje", ) , 
+
+            );
+
         $proyecto= Proyecto::find($idproy);
-        return view('someter/someter',compact('proyecto'));
+
+//0.Convocatoria en tiempo
+        $paymentDate = new \DateTime(); // Today
+        $paymentDate->format('d/m/Y'); // echos today! 
+
+        $contractDateBegin = new \DateTime($proyecto->Convocatoria->Fecha_inicio);
+        $contractDateEnd  = new \DateTime($proyecto->Convocatoria->Fecha_fin);
+        $validacion["convocatoria"]["rubro"] = "convocatoria";
+
+        if(
+          $paymentDate->getTimestamp() > $contractDateBegin->getTimestamp() && 
+          $paymentDate->getTimestamp() < $contractDateEnd->getTimestamp() ) {
+            $validacion["convocatoria"]["resultado"] = true;
+            $validacion["convocatoria"]["mensaje"] = "En tiempo ()";
+        }else{
+            $validacion["convocatoria"]["resultado"] = false;
+            $validacion["convocatoria"]["mensaje"] = "Sometido fuera del tiempo de la convocatoria";
+        }
+
+//1. Protocolo
+/*
+////protocolo
+resumen, introduccion, antecedentes, hipotesis, marco_teorico, 
+metas, objetivo_general, objetivos_especificos, impacto_beneficio, 
+metodologia, referencias, lugar, infraestrucutura
+*/
+
+
+//2. Colaboradores
+//3. Entregables
+//4. Cronograma
+//5. Presupuesto (financiado)
+        return view('someter/someter',compact('proyecto','validacion'));
     }
     
     public function update(Request $request, $idproy)
