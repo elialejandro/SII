464a149bd636b7cbfb96525bea1f46c4aaaf846f
Eli Alejandro <iscelialejandro@gmail.com>
2018-06-23 13:41:58 -0500

    Se actualizó a Bootstrap 4.1.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,9 @@
     @yield('styles')
     <!-- Scripts -->
     <!-- defer -->
-    <script src="{{ asset('js/app.js') }}"></script> 
+    <!-- script src="{{ asset('js/app.js') }}"></script --> 
     <script src="{{ asset('jquery/jquery-3.3.1.min.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="{{ asset('bootstrap/js/bootstrap.js')}}"></script>
     @yield('sctipts')
     <!-- Fonts -->
