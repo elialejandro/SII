c6117cabcf4eff4a184d2ee320d07f5700ed7874
I.S.C. Jorge Octavio Guzmán <jogs78@gmail.com>
2018-06-26 13:37:52 -0500

    CAMBIOS POR DOS PUNTOS

--- resources/views/sistema/welcome.blade.php
+++ resources/views/sistema/welcome.blade.php
@@ -75,7 +75,7 @@
                 </div>
             <div class="content">
                 <div class="title m-b-md">
-                    SISTEMA:
+                    SISTEMA:::
                 </div>
 
             </div>
